bfac71bb5c07320ec2f9c81473c83eeba777329d
LightsNtloko <ST10125711.edu.za>
2023-07-28 12:11:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Execute alphabets to lower case a-z
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (k <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar("\n");
+	return (0);
+}
