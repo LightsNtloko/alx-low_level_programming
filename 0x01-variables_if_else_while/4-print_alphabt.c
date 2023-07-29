588ae7ac3e42734facdfdeb103ea3c912a1be93d
LightsNtloko <ST10125711.edu.za>
2023-07-28 20:52:05 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets except q and e
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	while (a <= 'z')
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
