54f957efe4fb46e8b3fff7a249ae2f15255a7904
LightsNtloko <ST10125711.edu.za>
2023-08-01 04:19:40 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10
+ * times the alphabet in lowercas,
+ * followed by a new line.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
