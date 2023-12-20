d6f200b5eb061fe13bef7e48ba2637db7f1b0bf9
LightsNtloko <ST10125711@rcconnect.edu.za>
2023-12-19 21:04:49 -0800

    A function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,33 @@
  */
 void times_table(void)
 {
-	int row, col, product;
+	int x, y, z, l, n;
 
-
-	for (row = 0; row <= 9; row++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (col = 0; col <= 9; col++)
+		for (y = 0; y <= 9; y++)
 		{
-			product = row * col;
-			printf("%2d ", product);
+			z = x * y;
+			if (z > 9)
+			{
+				l = z % 10;
+				n = (z - l) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(n + '0');
+				_putchar(l + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }
