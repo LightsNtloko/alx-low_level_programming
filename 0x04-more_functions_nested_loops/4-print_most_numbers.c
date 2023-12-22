17e96793d98578fc38fdb64da9269c71c49a9c49
LightsNtloko <ST10125711@rcconnect.edu.za>
2023-12-21 19:19:02 -0800

    A function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print 0 to 9excluding 2 and 4
+ * print_most_numbers - print 0 to 9 excluding 2 and 4
  * Return: void
  */
-
 void print_most_numbers(void)
 {
 	int k;
 
-	for (k = 0; k < 10, k++)
+	for (k = 0; k < 10; k++)
 	{
-		if (k != 2 && k != 4)
-			
-			_putchar(k);
+		if (k != 2)
+		{
+			if (k != 10)
+			{
+				_putchar(k);
+			}
+		}
 	}
 	_putchar("\n");
 }
