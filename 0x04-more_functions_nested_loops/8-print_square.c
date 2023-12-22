f7c9cab3e172db72f98e00b2a7eb2110bd2f2398
LightsNtloko <ST10125711@rcconnect.edu.za>
2023-12-22 08:20:49 -0800

    A function that prints a square, followed by a new line

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,15 +4,15 @@
  * print_square - print a squre in the terminal
  * @size: size of a squre
  */
-void print_square(int area)
+void print_square(int size)
 {
-	if (area > 0)
+	if (size > 0)
 	{
 		int k, n;
 
-		for (k = 0; k < area; k++)
+		for (k = 0; k < size; k++)
 		{
-			for (n = 0; n < area; n++)
+			for (n = 0; n < size; n++)
 			{
 				_putchar('#');
 			}
