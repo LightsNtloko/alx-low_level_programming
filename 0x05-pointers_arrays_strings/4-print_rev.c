08a047b561d414d4985fcd0e36826f60b35abe38
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-01-03 12:32:13 -0800

    A function that prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * _print_rev - A function that prints a string in reverse using recursion
+ * @k: The string should be printed in reverse
+ */
+void _print_rev(char *k)
+{
+	if (*k == '\0')
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		return;
+	}
+	_print_rev(k + 1);
+	write(STDOUT_FILENO, k, 1);
+}
