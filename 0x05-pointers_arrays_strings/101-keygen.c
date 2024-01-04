c0a95a6bfd2713b2e940a34eaa1b629e36e95cc6
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-01-04 12:34:19 -0800

    A program that generates random valid passwords for the program 101-crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generate a random valid password for the 101-crackme program
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char password[101];
+	int k, length;
+
+	srand(time(NULL));
+
+	length = rand() % 15 + 6;
+
+	for (k = 0; k < length; ++k)
+	{
+		password[k] = rand() % ('~' - ' ' + 1) + ' ';
+	}
+
+	password[length] = '\0';
+
+	printf("Generated Password: %s\n", password);
+
+	return (0);
+}
