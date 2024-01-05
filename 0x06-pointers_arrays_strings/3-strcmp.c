f75da6027fe7e3b5049bf72bda885029ae8d47cb
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-01-05 06:33:14 -0800

    A unction that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,21 +2,20 @@
 
 /**
  * _strcmp - A function that compares two strings.
- * @s1: String to be compared with s2
- * @s2: String to be compared with s2
- * Return: Strings to be compared
+ * @s1: String to be compared with s2.
+ * @s2: String to be compared with s2.
+ * Return: Difference between the characters at a first
+ * non-matching position.
  */
 int _strcmp(char *s1, char *s2)
 {
-	char s1 = "Lights";
-	char s2 = "Ntloko";
 	int k;
 
-	for (k = 0; Lights[k] != '\0' && Ntloko[k] != '\0'; k++)
+	for (k = 0; s1[k] != '\0' && s2[k] != '\0'; k++)
 	{
-		if (Lights[k] != Ntloko[k])
+		if (s1[k] != s2[k])
 		{
-			return Lights[k] - Ntloko[k];
+			return (s1[k] - s2[k]);
 		}
 	}
 }
