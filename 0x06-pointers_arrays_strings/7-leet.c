2268b8fa5355129cb2fdd0162d2d463787d79992
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-01-05 12:46:10 -0800

    A function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,21 +9,29 @@
 char *leet(char *str)
 {
 	int k = 0;
+	char leet_map[] = {'4', '3', '0', '7', '1'};
+
 
 	while (str[k] != '\0')
 	{
-		if (str[k] == 'a' || str[k] == 'A')
-			str[k] = '4';
-		if (str[k] == 'e' || str[k] == 'E')
-			str[k] = '3';
-		if (str[k] == 'o' || str[k] == 'O')
-			str[k] = '0';
-		if (str[k] == 't' || str[k] == 'T')
-			str[k] = '7';
-		if (str[k] == 'l' || str[k] == 'L')
-			str[k] = '1';
+		int i = 0;
+
+		while (i < 5)
+		{
+			if ((str[k] == 'a' || str[k] == 'A') && i == 0)
+				str[k] = leet_map[i];
+			else if ((str[k] == 'e' || str[k] == 'E') && i == 1)
+				str[k] = leet_map[i];
+			else if ((str[k] == 'o' || str[k] == 'O') && i == 2)
+				str[k] = leet_map[i];
+			else if ((str[k] == 't' || str[k] == 'T') && i == 3)
+				str[k] = leet_map[i];
+			else if ((str[k] == 'l' || str[k] == 'L') && i == 4)
+				str[k] = leet_map[i];
 
 
+			i++;
+		}
 		k++;
 	}
 
