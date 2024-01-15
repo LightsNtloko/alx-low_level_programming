d3f27abbc79ed3955c2a38fcf06c3a427fdeb29d
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-01-15 13:53:50 -0800

    A C program that prints exactly a specific statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message to the standard output.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle,\n");
 
 	return (0);
 }
