cdf56ec36ab507566fc792f7a646c9b0dfda3fa9
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-01-31 01:45:48 -0800

    A program that prints its name, followed by a new line

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Program that prints my name
+ * @argc: count the number of arguments
+ * @argv: the argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
