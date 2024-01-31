90bbcc09cc7ca3c2e4169f1b4d6f3b38167cb2e5
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-01-31 02:06:39 -0800

    main.h

--- 0x09-static_libraries/0x0A-argc_argv/main.h
+++ 0x09-static_libraries/0x0A-argc_argv/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char k);
-int _atoi(char *k);
-int main (int argc, char *argv[]);
+int _putchar(char c);
+int _atoi(char *c);
+int main(int argc, char *argv[]);
 
 #endif
