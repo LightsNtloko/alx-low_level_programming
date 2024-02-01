036eee16fdf57a84fd2759e859502d13edefec06
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-02-01 08:31:00 -0800

    A function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdub - Function that returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: The string to be duplicated
+ *
+ * Return: A pointer to the duplicated string, or NULL if no enough memory.
+ */
+char *_strdup(char *str)
+{
+	unsigned int len, k;
+	char *dub_str;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len] != '\0'; len++);
+
+	dub_str = malloc((len + 1) * sizeof(char));
+
+	if (dub_str == NULL)
+		return (NULL);
+
+	for (k = 0; k <= len; k++)
+		dub_str[k] = str[k];
+
+	return (dub_str);
+}
