f42d80ee25804d8e6796d3c656e788424d6cfeb2
root <ST10125711@rcconnect.edu.za>
2024-03-21 23:54:08 -0700

    A function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,106 +1,97 @@
 #include "main.h"
 #include <stdlib.h>
 
+int word_length(char *str);
 int count_words(char *str);
-char *duplicate_word(char *str, int start, int end);
+char **strtow(char *str);
 
 /**
- * strtow - The function splits the string into words
- * @str: The imput string
- *
- * Return: A pointer to an array of strings (words), or NULL if it fails
+ * word_length - The function locates the index labeling the end of the first
+ * word in the string.
+ * @str: The input string to be searched
+ * Return: The index lableling the end of the initial word pointed to by str
  */
-char **strtow(char *str)
+int word_length(char *str)
 {
-	char **words;
-	int word_count, i, j, word_start, word_end;
+	int index = 0, length = 0;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
-
-	word_count = count_words(str);
-	if (word_count == 0)
-		return (NULL);
-
-	words = malloc((word_count + 1) * sizeof(char *));
-	if (words == NULL)
-		return (NULL);
-
-	j = 0;
-	for (i = 0; str[i] != '\0'; i++)
+	while (*(str + index) && *(str + index) != ' ')
 	{
-		if (str[i] != ' ')
+		length++;
+		index++;
+	}
+	return (length);
+}
+
+/**
+ * count_words - The function counts the number of words carried in the string
+ * @str: The input string to be searched
+ * Return: The number of words carried in the string
+ */
+int count_words(char *str)
+{
+	int index = 0, words = 0, length = 0;
+
+	for (index = 0; *(str + index); index++)
+		length++;
+
+	for (index = 0; index < length; index++)
+	{
+		if (*(str + index) != ' ')
 		{
-			word_start = i;
-			while (str[i] != ' ' && str[i] != '\0')
-				i++;
-			word_end = i;
-			words[j] = duplicate_word(str, word_start, word_end);
-			if (words[j] == NULL)
-			{
-				while (j > 0)
-				{
-					j--;
-					free(words[j]);
-				}
-				free(words);
-				return (NULL);
-			}
-			j++;
+			words++;
+			index += word_length(str + index);
 		}
 	}
-	words[j] = NULL;
 	return (words);
 }
 
 /**
- * count_words -The function counts the number of words in the string
- * @str: The imput string
+ * strtow - The function splits the string into words
+ * @str: The input string to be split
  *
- * Return: The number of words in the string
+ * Return: If string = NULL, string = "", or the function fails - NULL.
+ * Otherwise - return to an array of words.
  */
-int count_words(char *str)
+char **strtow(char *str)
 {
-	int count = 0, i = 0;
+	char **strings;
+	int index = 0, words, t, letters, c;
 
-	while (str[i] != '\0')
-	{
-		if (str[i] != ' ')
-		{
-			count++;
-			while (str[i] != ' ' && str[i] != '\0')
-				i++;
-		}
-		else
-		{
-			while (str[i] == ' ')
-				i++;
-		}
-	}
-	return (count);
-}
-
-/**
- * duplicate_word - The function that forms a duplicate of a word from a string
- * @str: The imput string
- * @start: The starting point of the word
- * @end: The ending point of the word
- *
- * Return: A pointer to the duplicated word, or NULL if it fails
- */
-char *duplicate_word(char *str, int start, int end)
-{
-	char *word;
-	int i, j;
-
-	word = malloc((end - start + 1) * sizeof(char));
-	if (word == NULL)
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
 
-	for (i = start, j = 0; i < end; i++, j++)
-		word[j] = str[i];
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
 
-	word[j] = '\0';
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
 
-	return (word);
+	for (t = 0; t < words; t++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_length(str + index);
+
+		strings[t] = malloc(sizeof(char) * (letters + 1));
+
+		if (strings[t] == NULL)
+		{
+			for (; t >= 0; t--)
+				free(strings[t]);
+
+			free(strings);
+			return (NULL);
+		}
+		for (c = 0; c < letters; c++)
+			strings[t][c] = str[index++];
+
+		strings[t][c] = '\0';
+	}
+	strings[t] = NULL;
+
+	return (strings);
 }
