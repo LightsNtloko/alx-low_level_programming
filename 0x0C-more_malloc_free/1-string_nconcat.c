b2e398694147636a96b81447c7adf168762e1ffd
root <ST10125711@rcconnect.edu.za>
2024-03-28 19:39:28 -0700

    A function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,37 +12,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
+	unsigned int length1 = 0, length2 = 0, i, j;
+	char *concat;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[i])
-		i++;
-	while (s2[k])
-		k++;
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
-		return (NULL);
-	k = 0;
-	while (j < l)
+	if (s1 != NULL)
 	{
-		if (l <= i)
-			str[j] = s1[j];
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
+		while (s1[length1] != '\0')
+			length1++;
 	}
-	str[j] = '\0';
-	return (str);
+	if (s2 != NULL)
+	{
+		while (s2[length2] != '\0')
+			length2++;
+	}
+	if (n >= length2)
+		n = length2;
+
+	concat = malloc(sizeof(char) * (length1 + n + 1));
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < length1; i++)
+		concat[i] = s1[i];
+	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
+
+	concat[i + j] = '\0';
+
+	return (concat);
 }
