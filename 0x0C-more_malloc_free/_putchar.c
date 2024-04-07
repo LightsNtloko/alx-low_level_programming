d70881e9f76039462e9bbdd68b322eac65633acb
root <ST10125711@rcconnect.edu.za>
2024-04-06 21:59:40 -0700

    A program that multiplies two positive numbers

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - The function that writes the character k to a stdout
+ * @k: The character to be printed
+ * Return: On success 1, otherwise -1
+ */
+int _putchar(char k)
+{
+	return (putchar(k));
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,11 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *_memset(void *k, int b, unsigned int n);
+void print_output(char *output);
+int is_digit(char k);
+void print_error(void);
+void multiply(char *num1, char *num2);
+int main(int argc, char *argv[]);
+int _putchar(char k);
 
 #endif /* MAIN_H */
