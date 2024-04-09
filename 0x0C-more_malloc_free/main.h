533a165139a6ec2b85918c1bf740676d242bdbcb
root <ST10125711@rcconnect.edu.za>
2024-04-08 17:50:45 -0700

    header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,11 +7,11 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *_memset(void *k, int b, unsigned int n);
-int is_digit(char *t);
+int is_digit(const char *t);
 void print_error(void);
-int _strlen(char *t);
+int _strlen(const char *t);
 int main(int argc, char *argv[]);
 int _putchar(char k);
-void multiply_strings(char *t1, char *t2);
+void multiply_strings(const char *t1, const char *t2);
 
 #endif /* MAIN_H */
