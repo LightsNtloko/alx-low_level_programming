aebf116282f27a43c886ffcfd177f3136a517c73
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-04-22 12:06:34 -0700

    A function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,102 +1,49 @@
 #include <stdarg.h>
 #include <stdio.h>
-
-/**
- * print_char - The function prints a character argument.
- * @c: The character to print
- * @sep: Whether to print a separator after the character
- */
-void print_char(char c, int sep)
-{
-	printf("%c", c);
-	if (sep)
-		printf(", ");
-}
-
-/**
- * print_int - The function prints an integer argument.
- * @num: The integer to print
- * @sep: Whether to print a separator after the integer
- */
-void print_int(int num, int sep)
-{
-	printf("%d", num);
-	if (sep)
-		printf(", ");
-}
-
-/**
- * print_float - The function prints a float argument.
- * @fnum: The float to print
- * @sep: Whether to print a separator after the float
- */
-void print_float(float fnum, int sep)
-{
-	printf("%f", fnum);
-	if (sep)
-		printf(", ");
-}
-
-/**
- * print_string - The functon prints a string argument.
- * @str: The string to print
- * @sep: Whether to print a separator after the string
- */
-void print_string(char *str, int sep)
-{
-	if (str == NULL)
-		printf("(nil)");
-	else
-		printf("%s", str);
-	if (sep)
-		printf(", ");
-}
-
 /**
  * print_all - Function prints arguments based on the provided format string
  * @format: A format string specifying the types of arguments to print
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
+	int i, scan_stat;
+
 	char *str;
-	int num;
-	float fnum;
+	va_list spc;
 
-	if (format == NULL)
-	{
-		printf("\n");
-		return;
-	}
+	va_start(spc, format);
 
-	va_start(args, format);
-	while (format && format[i])
+	i = 0;
+	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{
-			case 'c':
-				num = va_arg(args, int);
-				print_char(num, format[i + 1]);
-				break;
 			case 'i':
-				num = va_arg(args, int);
-				print_int(num, format[i + 1]);
+				printf("%d", va_arg(spc, int));
+				scan_stat = 0;
 				break;
 			case 'f':
-				fnum = va_arg(args, double);
-				print_float(fnum, format[i + 1]);
+				printf("%f", va_arg(spc, double));
+				scan_stat = 0;
+				break;
+			case 'c':
+				printf("%c", va_arg(spc, int));
+				scan_stat = 0;
 				break;
 			case 's':
-				str = va_arg(args, char *);
-				print_string(str, format[i + 1]);
+				str = va_arg(spc, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
 				break;
 			default:
-				i++;
-				continue;
+				scan_stat = 1;
+				break;
 		}
+		if (format[i + 1] != '\0' && scan_stat == 0)
+			printf(", ");
 		i++;
 	}
-	va_end(args);
 	printf("\n");
+	va_end(spc);
 }
