e05fc7261af62d33b7e78706a2761c3fea12db11
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-05-02 08:13:49 -0700

    handle for jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://raw.githubusercontent.com/LightsNtloko/alx-low_level_programming/master/0x18-dynamic_libraries.so
+wget -P https://raw.githubusercontent.com/LightsNtloko/alx-low_level_programming/master/0x18-dynamic_libraries/libhandle.so
 export LD_PRELOAD="$PWD/../libhandle.so"
