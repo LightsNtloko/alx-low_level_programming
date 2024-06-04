440a74c98bda7d25179b5770677173de57798c2e
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-06-04 12:43:14 -0700

    A function that reverses a listint_t linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,6 +5,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct listint_s
 {
 	int n;
@@ -23,5 +30,9 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 void _putchar(char c);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
