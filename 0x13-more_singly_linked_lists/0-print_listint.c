ca373be2162f39a402b8b2f41b04a9213c716f43
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-06-12 18:28:40 -0700

    A function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,60 +1,11 @@
-#include <unistd.h>
+#include <stdio.h>
 #include "lists.h"
-
-/**
- * _putchar - The function that writes a character to stdout
- * @c: The character to print
- *
- * Return: On success 1,
- * on error -1 is returned and errno is set appropriately
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
- * _puts - The function that prints a string to stdout
- * @str: The string to be printed
- */
-void _puts(char *str)
-{
-	while (*str)
-	{
-		_putchar(*str++);
-	}
-}
-
-/**
- * print_number - The function that prints an integer to stdout
- * @k: The integer to be printed
- */
-void print_number(int n)
-{
-	unsigned int num;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -n;
-	}
-	else
-	{
-		num = n;
-	}
-
-	if (num / 10)
-	{
-		print_number(num / 10);
-	}
-
-	_putchar((num % 10) + '0');
-}
 
 /**
  * print_listint - The function that print all the elements of a listint_t
  * liked list.
  * @h: The pointer to the head of the list
- * Return: The number of nodes
+ * Return: The number of nodes in the list
  */
 size_t print_listint(const listint_t *h)
 {
@@ -62,8 +13,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		print_number(h->n);
-		_putchar('\n');
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
